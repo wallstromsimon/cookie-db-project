a6bbdb3bf2198116934c6f9db32744c74cbf90fa
AdminAdam <adamdalentoft@live.se>
2016-04-03 15:45:16 +0200

    added init_tables
    adds the customers

---
+++ sql_files/init_tables.sql
@@ -0,0 +1,23 @@
+insert into Customer (UserName, Address)
+values ('Finkakor AB', 'Helsingborg');
+
+insert into Customer (UserName, Address)
+values ('Småbröd AB', 'Malmö');
+
+insert into Customer (UserName, Address)
+values ('Kaffebröd AB', 'Landskrona');
+
+insert into Customer (UserName, Address)
+values ('Bjudkakor AB', 'Ystad');
+
+insert into Customer (UserName, Address)
+values ('Kalaskakor AB', 'Trelleborg');
+
+insert into Customer (UserName, Address)
+values ('Partykakor AB', 'Kristianstad');
+
+insert into Customer (UserName, Address)
+values ('Gästkakor AB', 'Hässleholm');
+
+insert into Customer (UserName, Address)
+values ('Skånekakor AB', 'Perstorp');
