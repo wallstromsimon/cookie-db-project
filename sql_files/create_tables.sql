24db7e6e0f2dc0e20d1ff00fdf3355dd4628db0a
AdminAdam <adamdalentoft@live.se>
2016-04-03 15:18:43 +0200

    fixed typo

--- sql_files/create_tables.sql
+++ sql_files/create_tables.sql
@@ -1,26 +1,26 @@
-create table Ingredient{
+create table Ingredient(
     IngredientName  varchar(30),
     AmountLeft      int,
     primary key (IngredientName),
     foreign key (IngredientName) references RecipeItem(IngredientName) 
-    };
+    );
 
-create table RecipeItem{
+create table RecipeItem(
     CookieName      varchar(30),
     IngredientName  varchar(30),
     Amount          int,
     primary key (CookieName, IngredientName),
     foreign key (CookieName) references Cookie(CookieName),
     foreign key (IngredientName) references Ingredient(IngredientName)
-    };
+    );
 
-create table Cookie{
+create table Cookie(
     CookieName  varchar(30),
     primary key (CookieName),
     foreign key (CookieName) references RecipeItem(CookieName)
-    };
+    );
 
-create table Pallet{
+create table Pallet(
     PalletID    int,
     CookieName  varchar(30),
     DateMade    date,
@@ -28,9 +28,9 @@ create table Pallet{
     primary key (PalletID),
     foreign key (CookieName) references Cookie(CookieName),
     foreign key (PalletID) references DeliverdItem(PalletID)
-    };
+    );
 
-create table DeliverdItem{
+create table DeliverdItem(
     PalletID        int,
     OrderID         int,
     CustomerName    varchar(30),
@@ -38,28 +38,28 @@ create table DeliverdItem{
     primary key (PalletID,OrderID),
     foreign key (PalletID) references Pallet(PalletID),
     foreign key (OrderID) references Order(OrderID)
-    };
+    );
 
-create table Order{
+create table Order(
     OrderID         int,
     Customer        varchar(30),
     DeliveryDate    date,
     primary key (OrderID),
     foreign key (OrderID) references DeliverdItem(OrderID)
-    };
+    );
 
-create table Customer{
+create table Customer(
     UserName    varchar(30),
     Address     varchar(30),
     primary key (UserName)
-    };
+    );
 
-create table OrderedItem{
+create table OrderedItem(
     OrderID         int,
     CookieName      varchar(30),
     NbrPallets      int,
     primary key (OrderID,CookieName),
     foreign key (OrderID) references Order(OrderID)
     foreign key (CookieName) references Cookie(CookieName)
-    };    
+    );    
 
